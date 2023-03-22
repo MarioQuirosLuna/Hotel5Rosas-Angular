77ab693542d66a5da0b5e176d5067fb5e916f773
MarioQuirosL <marioql.dev@gmail.com>
2023-03-22 14:40:58 -0600

    Cambio el nombre del componente page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomePageComponent } from './components/interface/home-page/home-page.component';
+import { PageComponent } from './components/interface/page/page.component';
 
 const routes: Routes = [
-  { path: '**', component: HomePageComponent }
+  { path: '**', component: PageComponent }
 ];
 
 @NgModule({
