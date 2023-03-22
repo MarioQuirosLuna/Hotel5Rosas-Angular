5f965872c6502fa465ea185d6ed60be019ad21f4
Juanpa Marin <juanpamarinaguilar01@gmail.com>
2023-03-22 17:42:43 -0600

    Menu Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GenericPageComponent } from './components/Pages/generic-page/generic-page.component';
 import { HomePageComponent } from './components/Pages/home-page/home-page.component';
-import { AboutUsComponent } from './components/interface/about-us/about-us.component';
+import { AboutUsComponent } from './components/Pages/about-us/about-us.component';
+import { FacilitiesPageComponent } from './components/Pages/facilities-page/facilities-page.component';
 
 const routes: Routes = [
   { path: 'about-us', component: AboutUsComponent },
+  { path: 'home', component: HomePageComponent },
+  { path: 'facilities', component: FacilitiesPageComponent },
   { path: '**', redirectTo: "home" },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { HeaderComponent } from './components/interface/header/header.component'
 import { LateralMenuComponent } from './components/interface/lateral-menu/lateral-menu.component';
 import { InformationComponent } from './components/interface/information/information.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AboutUsComponent } from './components/interface/about-us/about-us.component';
+import { AboutUsComponent } from './components/Pages/about-us/about-us.component';
 import { HomePageComponent } from './components/Pages/home-page/home-page.component';
 import { FacilitiesPageComponent } from './components/Pages/facilities-page/facilities-page.component';
 
