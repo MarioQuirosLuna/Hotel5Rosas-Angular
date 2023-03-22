bdbf8db453e052dd2f905593fb876a2639ad419e
MarioQuirosL <marioql.dev@gmail.com>
2023-03-22 15:45:00 -0600

    Estructura de carpetas y creacion del home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { GenericPageComponent } from './components/interface/generic-page/generic-page.component';
+import { GenericPageComponent } from './components/Pages/generic-page/generic-page.component';
+import { HomePageComponent } from './components/Pages/home-page/home-page.component';
 
 const routes: Routes = [
-  { path: '**', component: GenericPageComponent }
+  { path: '**', component: HomePageComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GenericPageComponent } from './components/interface/generic-page/generic-page.component';
+import { GenericPageComponent } from './components/Pages/generic-page/generic-page.component';
 import { AreaPublicityComponent } from './components/interface/area-publicity/area-publicity.component';
 import { FooterComponent } from './components/interface/footer/footer.component';
 import { HeaderComponent } from './components/interface/header/header.component';
 import { LateralMenuComponent } from './components/interface/lateral-menu/lateral-menu.component';
 import { InformationComponent } from './components/interface/information/information.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomePageComponent } from './components/Pages/home-page/home-page.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     FooterComponent,
     HeaderComponent,
     LateralMenuComponent,
-    InformationComponent
+    InformationComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
