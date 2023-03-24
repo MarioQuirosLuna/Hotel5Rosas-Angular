d5d48ca28e6ddbb05fdc221fdd8b80c87fd6fb87
MarioQuirosLuna <marioql.dev@gmail.com>
2023-03-24 13:04:01 -0600

    Creacion de carrucel

--- src/app/components/interface/area-publicity/area-publicity.component.ts
+++ src/app/components/interface/area-publicity/area-publicity.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./area-publicity.component.css']
 })
 export class AreaPublicityComponent {
-
+  publicities = [
+    {
+      id: "1",
+      name: "img 1",
+      description: "url",
+      img: "https://picsum.photos/500/500",
+    },
+    {
+      id: "2",
+      name: "img 2",
+      description: "url",
+      img: "https://picsum.photos/500/500",
+    },
+    {
+      id: "3",
+      name: "img 3",
+      description: "url",
+      img: "https://picsum.photos/500/500",
+    }
+  ];
 }
