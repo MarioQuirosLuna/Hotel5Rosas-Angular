0a54a0b5076c8d71724a1758767f8e132275c891
MarioQuirosL <marioql.dev@gmail.com>
2023-03-26 13:11:43 -0600

    galeria con funcionalidad al darle click

--- src/app/components/Pages/about-us-page/about-us.component.ts
+++ src/app/components/Pages/about-us-page/about-us.component.ts
@@ -6,5 +6,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./about-us.component.css']
 })
 export class AboutUsComponent {
+  galeria = [
+    {
+      Imagen: 'https://picsum.photos/401/400',
+    },
+    {
+      Imagen: 'https://picsum.photos/402/400',
+    },
+    {
+      Imagen: 'https://picsum.photos/403/400',
+    },
+    {
+      Imagen: 'https://picsum.photos/404/400',
+    },
+    {
+      Imagen: 'https://picsum.photos/405/400',
+    },
+    {
+      Imagen: 'https://picsum.photos/406/400',
+    },
+    {
+      Imagen: 'https://picsum.photos/407/400',
+    },
+    {
+      Imagen: 'https://picsum.photos/408/400',
+    },
+    {
+      Imagen: 'https://picsum.photos/409/400',
+    },
+    {
+      Imagen: 'https://picsum.photos/410/400',
+    }
+  ];
+  ImagenDefault = this.galeria[0].Imagen;
+
+  constructor() { }
+
+  showImage(image: string) {
+    this.ImagenDefault = image;
+  }
+
 
 }
