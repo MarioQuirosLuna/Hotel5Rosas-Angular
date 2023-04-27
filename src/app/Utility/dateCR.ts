37f2927922c4361df68bb30bfe08eda57e51cb18
MarioQuirosL <marioql.dev@gmail.com>
2023-04-27 13:44:54 -0600

    Reservation with date and hour

--- src/app/Utility/dateCR.ts
+++ src/app/Utility/dateCR.ts
@@ -3,9 +3,9 @@ export function dateCR(): string {
     const costaRicaTimezoneOffset = -360; // Costa Rica is -6 hours behind UTC
     const costaRicaTime = new Date(now.getTime() + costaRicaTimezoneOffset * 60 * 1000);
 
-    const formattedDate = costaRicaTime.toISOString().slice(0, 10);
+    const formattedDate = costaRicaTime.toISOString().slice(0, 16);
 
-    // formattedDate now contains a string formatted as "yyyy-mm-dd"
+    // formattedDate now contains a string formatted as "yyyy-mm-ddThh:mm"
 
     return formattedDate;
 }
